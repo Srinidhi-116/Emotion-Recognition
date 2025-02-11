d4cad436061c86c2fb64b3c7c79073f2fce09f0a
Srinidhi-116 <srinidhi.b373@gmail.com>
2025-02-11 17:24:51 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,13 +1,13 @@
 function analyzeEmotion() {
     const inputText = document.getElementById('input').value;
-    fetch(` https://emorecog1.netlify.app/emotion?text=${encodeURIComponent(inputText)}`)
+    fetch(`emorecog1.netlify.app/emotion?text=${encodeURIComponent(inputText)}`)
         .then(response => response.json())
         .then(data => {
             const emoji = getEmojiForScore(data.score);
             document.getElementById('emojiDisplay').innerText = emoji;
         })
         .catch(error => {
-            console.error('Error:', error);
+            console.log('Error:'+ error);
         });
 }
 
